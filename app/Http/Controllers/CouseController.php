2d90d9e3df0a766896633aba7b1749a80738abf1
Hikkywannafly <76279331+TamNguyenS@users.noreply.github.com>
2022-05-28 20:39:44 +0700

    first commit

---
+++ app/Http/Controllers/CouseController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Couse;
+use App\Http\Requests\StoreCouseRequest;
+use App\Http\Requests\UpdateCouseRequest;
+
+class CouseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreCouseRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreCouseRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Couse  $couse
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Couse $couse)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Couse  $couse
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Couse $couse)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateCouseRequest  $request
+     * @param  \App\Models\Couse  $couse
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateCouseRequest $request, Couse $couse)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Couse  $couse
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Couse $couse)
+    {
+        //
+    }
+}
